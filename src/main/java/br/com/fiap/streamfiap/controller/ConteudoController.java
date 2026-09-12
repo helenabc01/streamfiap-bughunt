138c0ed5d6a96a056b386a6b98c3091ed35ec28f
lenabc01 <helenabarcosta01@gmail.com>
2026-09-11 22:31:49 -0300

    refactor: clean05 - remove unused commented code

--- src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
+++ src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
@@ -74,10 +74,4 @@ public class ConteudoController {
         return ResponseEntity.status(201).body(conteudoRepository.save(novo));
     }
 
-    // TODO: reativar quando confirmarem a regra de cupons (não apagar, pode ser
-    // útil)
-    // if (usuario.temCupomAtivo()) {
-    // preco = preco - 5.0;
-    // aplicarPromocao();
-    // }
 }
